a382f69c5ed79505f0d912e91b16e561ee7c7496
Richard Deloge <richarddeloge@gmail.com>
2020-09-16 22:49:56 +0200

    test gitlab ci

--- ci/before_script.sh
+++ ci/before_script.sh
@@ -15,4 +15,3 @@ php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
 php -r "if (hash_file('SHA384', 'composer-setup.php') === file_get_contents('installer.sig')) { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
 php composer-setup.php
 php -r "unlink('composer-setup.php'); unlink('installer.sig');"
-mv composer.phar composer
