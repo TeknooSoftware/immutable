360a0cd7a32d4671d24a7c90f4f6853fc19d31c2
Richard Deloge <richarddeloge@gmail.com>
2020-09-17 11:00:09 +0200

    test gitlab ci

--- ci/before_script.sh
+++ ci/before_script.sh
@@ -13,5 +13,5 @@ apt-get install git wget -yqq
 wget https://composer.github.io/installer.sig -O - -q | tr -d '\n' > installer.sig
 php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
 php -r "if (hash_file('SHA384', 'composer-setup.php') === file_get_contents('installer.sig')) { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
-php composer-setup.php
+php composer-setup.php --install-dir /usr/local/bin --filename=composer
 php -r "unlink('composer-setup.php'); unlink('installer.sig');"
